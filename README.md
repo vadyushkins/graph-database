f05ea1a29d6c2736b6203bde5c4872daa5ddb040
viabzalov <vadim.i.abzalov@gmail.com>
2020-09-06 20:50:24 +0300

    Added installation script, travis ci and simple suite tests
    Added simple ci
    
    Updated install
    
    Added simple tests for suites
    
    Updated install script
    
    Updated installation
    
    Updated travis installation
    
    Updated installation
    
    Updated installation
    
    Updated installation
    
    Updated installation
    
    Updated installation
    
    Updated installation
    
    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # graph-database
 Simple graph database on Python
+
+# CI
+
+[![Build Status](https://travis-ci.com/viabzalov/graph-database.svg?branch=dev)](https://travis-ci.com/viabzalov/graph-database)
+[![Build Status](https://travis-ci.com/viabzalov/graph-database.svg?branch=master)](https://travis-ci.com/viabzalov/graph-database)
+
+# Install
+```bash
+bash install.sh
+```
---
+++ install.sh
@@ -0,0 +1,16 @@
+conda --version
+if [[ $? -ne 0 ]]; then
+  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+  bash miniconda.sh -b -p $HOME/miniconda
+  source "$HOME/miniconda/etc/profile.d/conda.sh"
+  hash -r
+  conda config --set always_yes yes --set changeps1 no
+  conda update -q conda
+  # Useful for debugging any issues with conda
+  conda info -a
+fi
+conda config --add channels conda-forge
+conda create -q -n graph-database-environment python=3.8 pygraphblas pytest
+conda activate graph-database-environment
+conda install pygraphblas
+pip install -r requirements.txt
