80fc388599b08cf7d2ea977eb10eecdf192242bb
viabzalov <vadim.i.abzalov@gmail.com>
2020-09-06 23:07:17 +0300

    Updated install script

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
 conda config --add channels conda-forge
 conda install pygraphblas
 pip3 install pyformlang
