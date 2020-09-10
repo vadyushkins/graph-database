f8577baa2783e000443124a81e8629999ad8695b
viabzalov <vadim.i.abzalov@gmail.com>
2020-09-10 18:12:23 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,10 @@ if [[ $? -ne 0 ]]; then
   wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
   bash miniconda.sh -b -p $HOME/miniconda
   source "$HOME/miniconda/etc/profile.d/conda.sh"
+  conda init
+  source "$HOME/.bashrc"
   hash -r
-  conda config --set always_yes yes --set changeps1 no
+  conda config --set always_yes yes --set changeps1 yes
   conda update -q conda
   # Useful for debugging any issues with conda
   conda info -a
