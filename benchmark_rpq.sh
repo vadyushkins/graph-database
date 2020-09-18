a20d1dce08ca0b69348fe08b228c081893719af1
viabzalov <vadim.i.abzalov@gmail.com>
2020-09-18 22:23:46 +0300

    Debuggin install.sh on travis

--- benchmark_rpq.sh
+++ benchmark_rpq.sh
@@ -1,4 +1,3 @@
-conda activate test-environment || activate test-environment
 rm -rf .benchmarks
 python3 -m pytest -v -s tests/benchmark_rpq/test_benchmark_rpq.py -m --benchmark-autosave --benchmark-save-data
 pytest-benchmark compare --name=short --csv=RPQ
