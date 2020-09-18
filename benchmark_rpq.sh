72c4975e15cfc34c4a0bf338a3efd9b38a4f95e6
viabzalov <vadim.i.abzalov@gmail.com>
2020-09-18 22:33:26 +0300

    Debuggin install.sh on travis

--- benchmark_rpq.sh
+++ benchmark_rpq.sh
@@ -24,6 +24,7 @@ pip3 install --upgrade pip
 pip install -r requirements.txt
 cd tests/benchmark_rpq/ ; gdown https://drive.google.com/uc?id=1ZZ8FI6MxQ2rWIRxBQjZOVw64zHdbxcmm ; cd ../../
 rm -rf .benchmarks
-python3 -m pytest -v -s tests/benchmark_rpq/test_benchmark_rpq.py -m --benchmark-autosave --benchmark-save-data
+pwd
+python3 -m pytest -v -s tests/benchmark_rpq/test_benchmark_rpq.py --benchmark-autosave --benchmark-save-data
 pytest-benchmark compare --name=short --csv=RPQ
 cat RPQ.csv
