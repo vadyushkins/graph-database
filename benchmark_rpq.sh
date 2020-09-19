4dd2ce36c75ade01ed82241a2f8c5213e8fb91cf
viabzalov <vadim.i.abzalov@gmail.com>
2020-09-19 10:38:40 +0300

    Debuggin install.sh on travis

--- benchmark_rpq.sh
+++ benchmark_rpq.sh
@@ -1,30 +1,7 @@
-conda --version
-if [[ $? -ne 0 ]]; then
-  case "$OSTYPE" in
-    linux*)  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh ;;
-    darwin*) wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh ;;
-  esac
-
-  bash ./miniconda.sh -b -p $HOME/miniconda
-  export PATH=$HOME/miniconda:$PATH
-  source "$HOME/miniconda/etc/profile.d/conda.sh"
-  hash -r
-  conda init bash
-  conda config --set always_yes yes --set changeps1 yes
-  conda update -q conda
-  # Useful for debugging issues with conda
-  conda info -a
-fi
-conda config --add channels conda-forge
-conda create -q -n test-environment python=3.8
-conda activate test-environment || activate test-environment
-conda install pygraphblas
-export PYTHONPATH="${PYTHONPATH}:./"
-pip3 install --upgrade pip
-pip install -r requirements.txt
+bash install.sh
 cd tests/benchmark_rpq/ ; gdown https://drive.google.com/uc?id=1ZZ8FI6MxQ2rWIRxBQjZOVw64zHdbxcmm ; cd ../../
 rm -rf .benchmarks
 pwd
-sudo nice -n -20 python3 -m pytest -v -s tests/benchmark_rpq/test_benchmark_rpq.py --benchmark-autosave --benchmark-save-data &
+python3 -m pytest -v -s tests/benchmark_rpq/test_benchmark_rpq.py --benchmark-autosave --benchmark-save-data
 pytest-benchmark compare --name=short --csv=RPQ
 cat RPQ.csv
--- tests/benchmark_rpq/test_benchmark_rpq.py
+++ tests/benchmark_rpq/test_benchmark_rpq.py
@@ -2,7 +2,7 @@ import pytest
 
 from src.RegularPathQuering import rpq
 
-from conftest import params
+from .conftest import params
 
 
 @pytest.mark.parametrize('impl,graph,regex', params)
@@ -10,4 +10,4 @@ def test_benchmark_rpq(impl, graph, regex, benchmark):
     g = impl.from_txt(graph)
     r = impl.from_regex(regex)
 
-    benchmark.pedantic(rpq, (g, r), rounds=3, iterations=3, warmup_rounds=3)
+    benchmark.pedantic(rpq, (g, r), rounds=1, iterations=1, warmup_rounds=0)
