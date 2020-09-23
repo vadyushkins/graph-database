2e76fd2941d18699530d4be29a2efd6e19cf8420
viabzalov <vadim.i.abzalov@gmail.com>
2020-09-23 10:24:22 +0300

    Smarter benchmark_rpq.sh

--- benchmark_rpq.sh
+++ benchmark_rpq.sh
@@ -1,4 +1,6 @@
-cd benchmarks/benchmark_rpq/
-gdown https://drive.google.com/uc?id=19L7RUCJlkgWQpQRnp6hMb7MLXibB4jTp
-cd ../../
+if [ -f benchmarks/benchmark_rpq/myDataForRPQ.zip ]; then
+  cd benchmarks/benchmark_rpq/
+  gdown https://drive.google.com/uc?id=19L7RUCJlkgWQpQRnp6hMb7MLXibB4jTp
+  cd ../../
+fi
 python3 -m pytest -v -s benchmarks/benchmark_rpq/test_benchmark_rpq.py -m "(LUBM300) or (LUBM500)"
