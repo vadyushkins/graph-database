137940924ba264c612ecca20ad0c03ca41339e5c
Vadim Abzalov <vadim.i.abzalov@gmail.com>
2020-09-24 08:59:26 +0300

    Added benchmarks results and analytics

--- benchmark_rpq.sh
+++ benchmark_rpq.sh
@@ -3,4 +3,11 @@ if [ ! -f benchmarks/benchmark_rpq/myDataForRPQ.zip ]; then
   gdown https://drive.google.com/uc?id=19L7RUCJlkgWQpQRnp6hMb7MLXibB4jTp
   cd ../../
 fi
-python3 -m pytest -v -s benchmarks/benchmark_rpq/test_benchmark_rpq.py -m "(LUBM300) or (LUBM500)"
+
+declare -a graphs=("LUBM300" "LUBM500" "LUBM1M" "LUBM1.5M" "LUBM1.9M" "geospecies" "proteomes" "mappingbased_properties_en" "uniprotkb_archea_asgard_group_1935183_0")
+
+for graph in "${graphs[@]}"; do
+  echo "Started benchmark $graph"
+  python3 -m pytest -v -s benchmarks/benchmark_rpq/test_benchmark_rpq.py -m "$graph"
+  echo "Ended benchmark $graph"
+done
