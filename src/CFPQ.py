99f28afc27319ffc6caa3f8a1454310cbbfc4a72
viabzalov <vadim.i.abzalov@gmail.com>
2020-09-28 12:48:46 +0300

    Optimized unit productions

--- src/CFPQ.py
+++ src/CFPQ.py
@@ -27,18 +27,19 @@ def cfpq(g: LabeledGraph, gr: MyCNF) -> Matrix:
             variables_for_edge[(i, i)] = variables_for_edge.get((i, i), set()) | {gr.start_symbol}
             edges_for_variable[gr.start_symbol] = edges_for_variable.get(gr.start_symbol, set()) | {(i, i)}
 
+    for p in gr.productions:
+        if len(p.body) == 1:
+            for i, j in edges_for_variable.get(p.body[0], set()):
+                if (i, j) not in edges_for_variable.get(p.head, set()):
+                    edges_for_variable[p.head] = edges_for_variable.get(p.head, set()) | {(i, j)}
+                    variables_for_edge[(i, j)] = variables_for_edge.get((i, j), set()) | {p.head}
+                    cur[p.head][i, j] = True
+
     changing = True
     while changing:
         changing = False
         for p in gr.productions:
-            if len(p.body) == 1:
-                for i, j in edges_for_variable.get(p.body[0], set()):
-                    if (i, j) not in edges_for_variable.get(p.head, set()):
-                        changing = True
-                        edges_for_variable[p.head] = edges_for_variable.get(p.head, set()) | {(i, j)}
-                        variables_for_edge[(i, j)] = variables_for_edge.get((i, j), set()) | {p.head}
-                        cur[p.head][i, j] = True
-            elif len(p.body) == 2:
+            if len(p.body) == 2:
                 for i, k in edges_for_variable.get(p.body[0], set()):
                     for l, j in edges_for_variable.get(p.body[1], set()):
                         if k == l:
