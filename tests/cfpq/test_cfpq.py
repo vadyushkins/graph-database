fa652ea5ac03a31f79eb6d0073b407a19fa25410
viabzalov <vadim.i.abzalov@gmail.com>
2020-11-01 21:50:30 +0300

    Added Recursive State Machine

--- tests/cfpq/test_cfpq.py
+++ tests/cfpq/test_cfpq.py
@@ -1,16 +1,11 @@
-from src.LabeledGraph import LabeledGraph
-from src.MyCNF import MyCNF
-
-
 def test_manual(manual_suite, algo, tmp_path):
     graph_file = tmp_path / 'graph.txt'
-    graph_file.write_text('\n'.join(manual_suite['edges']))
+    graph_file.write_text('\n'.join(manual_suite['graph']))
 
-    g = LabeledGraph.from_txt(graph_file)
+    grammar_file = tmp_path / 'grammar.txt'
+    grammar_file.write_text('\n'.join(manual_suite['grammar']))
 
-    gr = MyCNF.from_text(manual_suite['cnf'])
-
-    actual = algo(g, gr)
+    actual = algo(graph_file, grammar_file).cfpq()
 
     expected = manual_suite['expected']
 
